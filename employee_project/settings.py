eba8b8269d0fb3ded4f0e77f47c4d60bbaf43ab5
Sujin Shin <sujins2@illinois.edu>
2025-06-29 03:21:47 -0500

    Set postgres DB

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'employee_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": os.environ['DB_ADMIN'],
+        "PASSWORD": os.environ['DB_ADMIN_PW'],
+        "HOST": 'localhost', # "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 
