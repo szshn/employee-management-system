6e30a9b1617b25f666581d03612015ba967b9e80
Sujin Shin <sujins2@illinois.edu>
2025-06-30 23:50:35 -0500

    Refactor evaluate models and admin interface

--- evaluations/admin.py
+++ evaluations/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from .models import Attendance, Performance
 
-# Register your models here.
-admin.site.register(Attendance)
-admin.site.register(Performance)
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'date', 'status')
+    list_filter = ('date', 'status')
+    search_fields = ('employee__user__first_name', 'employee__user__last_name')
+    ordering = ('-date',)
+
+@admin.register(Performance)
+class PerformanceAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'rating', 'review_date')
+    list_filter = ('rating', 'review_date')
+    search_fields = ('employee__user__first_name', 'employee__user__last_name')
+    ordering = ('-review_date',)
--- evaluations/models.py
+++ evaluations/models.py
@@ -15,21 +15,20 @@ class Attendance(models.Model):
     status = models.CharField(max_length=7, choices=Status.choices)
     
     def __str__(self):
-        return f"{self.employee.get_full_name()} - Attendance ({self.date})"    # type: ignore
+        return f"{self.employee.user.get_full_name()} - {self.status} - ({self.date})"    # type: ignore
     
 class Performance(models.Model):
-    RATING_CHOICES = [
-        (1, '1 -- Poor'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5 -- Above expectation')
-    ]
+    RATING_CHOICES = [(i,i) for i in range(1,6)]
+
+    rating = models.IntegerField(
+        choices=RATING_CHOICES,
+        default=5,  # type: ignore
+        help_text="1 = Poor, 5 = Excellent"
+    )
     
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=False)
-    rating = models.IntegerField(choices=RATING_CHOICES, default=5) # type: ignore
     review_date = models.DateField("Performance review date", default=timezone.now)
     # TODO: Add reviewed by and comment fields
     
     def __str__(self):
-        return f"{self.employee.get_full_name()} - Performance ({self.review_date})"    # type: ignore
+        return f"{self.employee.user.get_full_name()} - {self.rating} - ({self.review_date})"    # type: ignore
