04eed547d1083ac646d22e47d639f70ec057cd8b
Sujin Shin <shin369123@gmail.com>
2025-08-09 20:22:54 -0500

    Change root endpoint permission and response content

--- employee_project/urls.py
+++ employee_project/urls.py
@@ -1,21 +1,32 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def api_root(request, format=None):
-    return Response(
-        {
+    return Response({
+        "API": {
             'Employees': reverse('employee-list', request=request, format=format),
             'Departments': reverse('department-list', request=request, format=format),
             'Attendances': reverse('attendance-list', request=request, format=format),
             'Performances': reverse('performance-list', request=request, format=format),
+        },
+        "Login": {
+            'Get Token': reverse('token_obtain_pair', request=request, format=format),
+            'Refresh Token': reverse('token_refresh', request=request, format=format),
+        },
+        "Docs": {
+            'Swagger': reverse('swagger', request=request, format=format),
+            'ReDoc': reverse('redoc', request=request, format=format),
+            'Download schema': reverse('schema', request=request, format=format)
         }
-    )
+    })
 
 
 urlpatterns = [
@@ -25,11 +36,11 @@ urlpatterns = [
     path('', include('evaluations.urls')),
     
     # path('api-auth/', include('rest_framework.urls')),    # for session authentication
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),     # for JWT authentication
-    path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    # for JWT authentication
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),     # for JWT authentication
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    # for JWT authentication
     
     # drf-spectacular URLs
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('docs/schema', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+    path('docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]   
